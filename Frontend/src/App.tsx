c74d8c8a378331bd6316965b2832df48ca735bab
Sena.Nguyen <Sena.Nguyen@avepoint.com>
2023-08-11 00:39:36 +0700

    update logic login

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,24 +1,22 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import Layout from './components/Layout/Layout';
-import Login from './pages/Login/Login';
-import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react'
+import './App.css'
+import Layout from './components/Layout/Layout'
+import Login from './pages/Login/Login'
+import { useNavigate } from 'react-router-dom'
 
 function App() {
-  const [user, setUser] = useState<boolean>();
-  const navigate = useNavigate();
+  const [user, setUser] = useState<boolean>()
+  const navigate = useNavigate()
   useEffect(() => {
-    const user = localStorage.getItem('accessToken');
+    const user = localStorage.getItem('accessToken')
     if (user) {
-      setUser(true);
-      navigate('/');
+      setUser(true)
     } else {
-      setUser(false);
-      navigate('/login');
+      setUser(false)
+      navigate('/login')
     }
-  }, [navigate]);
-  console.log(user);
-  return <div id="app">{user ? <Layout /> : <Login />}</div>;
+  }, [navigate])
+  return <div id='app'>{user ? <Layout /> : <Login />}</div>
 }
 
-export default App;
+export default App
--- Frontend/src/components/Layout/Layout.tsx
+++ Frontend/src/components/Layout/Layout.tsx
@@ -1,46 +1,41 @@
-import {
-  FolderOutlined,
-  HomeOutlined,
-  ReadOutlined,
-  UnorderedListOutlined,
-} from '@ant-design/icons';
-import { Menu } from 'antd';
-import { Route, Routes, useNavigate } from 'react-router-dom';
-import Homepage from '~/pages/Homepage/Homepage';
-import ManagementProduct from '~/pages/ManagementProduct/ManagementProduct';
-import ManagementStudent from '~/pages/ManagementStudent/ManagementStudent';
-import Personalinfo from '~/pages/PersonalInfo/Personalinfo';
-import Headertop from '../Headertop/Headertop';
+import { FolderOutlined, HomeOutlined, ReadOutlined, UnorderedListOutlined } from '@ant-design/icons'
+import { Menu } from 'antd'
+import { Route, Routes, useNavigate } from 'react-router-dom'
+import Homepage from '~/pages/Homepage/Homepage'
+import ManagementProduct from '~/pages/ManagementProduct/ManagementProduct'
+import ManagementStudent from '~/pages/ManagementStudent/ManagementStudent'
+import Personalinfo from '~/pages/PersonalInfo/Personalinfo'
+import Headertop from '../Headertop/Headertop'
 
 function Layout() {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
   const items = [
     {
       label: 'Trang chủ',
       key: '/',
-      icon: <HomeOutlined />,
+      icon: <HomeOutlined />
     },
     {
       label: 'Sinh viên',
       key: '/student',
-      icon: <UnorderedListOutlined />,
+      icon: <UnorderedListOutlined />
     },
     {
       label: 'Quản lí sinh viên',
       key: '/studentmanagement',
-      icon: <FolderOutlined />,
+      icon: <FolderOutlined />
     },
     {
       label: 'Quản lí sản phẩm',
       key: '/productmanagement',
-      icon: <FolderOutlined />,
+      icon: <FolderOutlined />
     },
     {
       label: 'Bảng tin',
       key: '/newfeed',
-      icon: <ReadOutlined />,
-    },
-  ];
+      icon: <ReadOutlined />
+    }
+  ]
   return (
     <div>
       <Headertop />
@@ -51,7 +46,7 @@ function Layout() {
             if (key === 'signout') {
               //test
             } else {
-              navigate(key);
+              navigate(key)
             }
           }}
           items={items}
@@ -59,24 +54,18 @@ function Layout() {
         <Content />
       </div>
     </div>
-  );
+  )
 }
 function Content() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Homepage />}></Route>
-        <Route
-          path="/studentmanagement"
-          element={<ManagementStudent />}
-        ></Route>
-        <Route
-          path="/productmanagement"
-          element={<ManagementProduct />}
-        ></Route>
-        <Route path="/personalinfo" element={<Personalinfo />}></Route>
+        <Route path='/' element={<Homepage />}></Route>
+        <Route path='/studentmanagement' element={<ManagementStudent />}></Route>
+        <Route path='/productmanagement' element={<ManagementProduct />}></Route>
+        <Route path='/personalinfo' element={<Personalinfo />}></Route>
       </Routes>
     </div>
-  );
+  )
 }
-export default Layout;
+export default Layout
