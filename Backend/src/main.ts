aebd95e44df35571ba358e652c8b887242530014
anhtp524 <61215691+anhtp524@users.noreply.github.com>
2023-08-19 10:34:23 +0700

    Update folder

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -4,6 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('v1')
   const config = new DocumentBuilder()
     .setTitle('Managerment Lab')
     .setDescription('The Managerment Lab API description')
